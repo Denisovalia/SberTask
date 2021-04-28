a80763f3067dfa143155bba8bd66225b7e6118b2
Denisova Julia <pletnevajulia50905@gmail.com>
2021-04-28 14:15:06 +0300

    Add task for Sber

---
+++ EX2/EX2/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace EX2
+{
+    class HC
+    {
+        private double AdvPrc;
+        private int MinWDays;
+        public HC(double p, int d)
+        {
+            MinWDays = d;
+            AdvPrc = p;
+        }
+        public double get_AdvPrc()
+        {
+            return AdvPrc;
+        }
+        public int get_MinWDays()
+        {
+            return MinWDays;
+        }
+    }
+
+    class Employee
+    {
+        int salary;
+        DateTime day_begin;
+        DateTime day_end;
+        int plan_days;
+
+        public Employee(int s, DateTime b, DateTime e, int p)
+        {
+            salary = s;
+            day_begin = b;
+            day_end = e;
+            plan_days = p;
+        }
+
+        public int WorkDays()
+        {
+            TimeSpan span = day_end - day_begin;
+            return span.Days;
+        }
+        public void set_DayEnd(DateTime d)
+        {
+            day_end = d;
+        }
+        public int GetSalary()
+        {
+            return salary;
+        }
+
+        public int PlanDays()
+        {
+            return plan_days;
+        }
+
+        public DateTime Emp(string flag)
+        {
+            if (flag == "day_begin")
+                return day_begin;
+            return day_end;
+
+        }
+    }
+    class Program
+    {
+        
+        static void Formula_Adv(Employee emp, HC hc, DateTime DE)
+        {
+            double Prc = hc.get_AdvPrc();
+            bool UseAdvDay = false;
+            
+            if (Prc == 0)
+                return;
+            if (UseAdvDay == true)
+                emp.set_DayEnd(DE);
+            DateTime Ade = emp.Emp("day_end");
+            if (Ade < emp.Emp("day_end"))
+                return;
+
+            int WD = emp.WorkDays();
+
+            int MinWDays = hc.get_MinWDays();
+            if (WD < MinWDays)
+                return;
+
+           int Salary =  emp.GetSalary();
+            int PD = emp.PlanDays();
+
+            double result = Salary / PD * WD * Prc / 100;
+            Console.WriteLine(result);
+
+        }
+        static void Main(string[] args)
+        {
+            DateTime D1 = new DateTime(2020, 4, 4);
+            DateTime D2 = new DateTime(2021, 4, 4);
+            Employee Ivan = new Employee(10000, D1, D2, 20);
+            HC hc = new HC(0.1, 1);
+            Formula_Adv(Ivan,hc, new DateTime(2021, 4, 4));
+        }
+    }
+}
---
+++ Ex1.A/Ex1.A/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Ex1.A
+{
+    class Program
+    {
+
+        static void task1(int day, int month, int year, int start_sick, int end_sick)
+        {
+            int sum = 0;
+            int N = 0;
+            N = DateTime.DaysInMonth(year, month);
+
+            for (int i = day; i <= N; i++)
+            {
+                DateTime date1 = new DateTime(year, month, i);
+                if (start_sick == i)
+                {
+                    i = end_sick;
+                    continue;
+                }
+                if (date1.DayOfWeek != DayOfWeek.Sunday && date1.DayOfWeek != DayOfWeek.Saturday)
+                {
+
+                    if (i < 20)
+                        sum = sum + 200;
+                    else
+                        sum = sum + 300;
+                }
+
+            }
+
+            Console.WriteLine(sum);
+        }
+
+
+        static void Main(string[] args)
+        {
+            
+            
+            string input;
+            string date;
+            int month = 0;
+            int year = 0;
+            int day;
+            int start_sick;
+            int end_sick;
+            Console.WriteLine("Введите день начала отсчета");
+            day = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите месяц от 1 до 12");
+            month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите год");
+            year = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите начало больничного (если нет больничного введите 0)");
+            start_sick = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите конец больничного (если нет больничного введите 0)");
+            end_sick = int.Parse(Console.ReadLine());
+            task1(day, month, year, start_sick, end_sick);
+           
+        }
+    }
+}
